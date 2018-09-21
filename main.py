aa44a480d1e2d0865ed0cbca68b33fdf91aef7ff
Jeff Morice <jmorice.anim@gmail.com>
2018-09-20 20:31:26 -0500

    Added correct str.format replacement capability to the index page and the encryped_input page.

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
     <html>
         <head>
             <style>
-                form {
+                form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
             </style>
         </head>
         <body>
@@ -33,17 +33,18 @@ form = """
                 </label>
                 <input id="rot" type="text" name="rot" value="0"/>
                 <br>
-                <textarea name="text"></textarea>
+                <textarea name="text">{0}</textarea>
                 <br>
                 <input type="submit" />
             </form>
         </body>
     </html>
     """
+    #form = form.format()
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['post'])
 def encrypt_input():
@@ -51,7 +52,8 @@ def encrypt_input():
     rotation = int(rotation)
     user_text = request.form["text"]
 
-    return "<h1>" + encrypt(user_text, rotation) + "</h1>"
+    #return "<h1>" + encrypt(user_text, rotation) + "</h1>"
     #return "<h1>" + str(rotation) + "</h1>"
+    return form.format(encrypt(user_text, rotation))
 
 app.run()
